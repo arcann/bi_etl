cce328ff47bdbda2163b10f6d046c640dcf7d6a1
Derek Wood <derek.wood@datastrong.com>
2017-06-21 09:21:04 -0400

    Config parser do not change directory

--- bi_etl/bi_config_parser.py
+++ bi_etl/bi_config_parser.py
@@ -49,10 +49,6 @@ class BIConfigParser(ConfigParser):
                                                    'trace_setup',
                                                    fallback=False
                                                    )
-        # Make sure the current working dir is the bi_etl home dir (where config.ini can be found)
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        dir_path = os.path.dirname(dir_path)
-        os.chdir(dir_path)
 
     def merge_parent(self, directories, parent_file):
         parent_config = ConfigParser(allow_no_value=True,
@@ -85,7 +81,7 @@ class BIConfigParser(ConfigParser):
                 if setting.startswith('parent'):
                     self.merge_parent(directories, self['Config'][setting])
 
-    def read_config_ini(self):
+    def read_config_ini(self, file_name='config.ini'):
         r"""
         If the BI_ETL_CONFIG environment variable is set, read the config file(s) as specified there (; delimited).
 
@@ -108,13 +104,13 @@ class BIConfigParser(ConfigParser):
             user_dir = os.path.expanduser('~')
             expected_config_files = [
                 # These static paths are for running as a windows service
-                os.path.join("C:\\", "bi_etl_config", "config.ini"),
-                os.path.join("D:\\", "bi_etl_config", "config.ini"),
-                os.path.join("E:\\", "bi_etl_config", "config.ini"),
+                os.path.join("C:\\", "bi_etl_config", file_name),
+                os.path.join("D:\\", "bi_etl_config", file_name),
+                os.path.join("E:\\", "bi_etl_config", file_name),
                 # If those don't work then look in the user directory
-                os.path.join(user_dir, 'bi_etl_config', 'config.ini'),
+                os.path.join(user_dir, 'bi_etl_config', file_name),
                 # Finally use a file in the current directory (will override other settings)
-                os.path.join(os.getcwd(), "config.ini"),
+                os.path.join(os.getcwd(), file_name),
             ]
         files_read = self.read(expected_config_files)
         if files_read is None or len(files_read) == 0:
