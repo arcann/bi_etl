c5f70fbec82529af71b7f58325b392e0e065e3d1
Derek Wood <arcann@users.noreply.github.com>
2018-09-13 11:38:10 -0400

    Add psycopg2_sql_extract

--- bi_etl/utility/postgresql/psycopg2_helpers.py
+++ bi_etl/utility/postgresql/psycopg2_helpers.py
@@ -1,13 +1,31 @@
 import psycopg2
 
 
-def psycopg2_extract(dbname, username, password, table_name, output_file_path, delimiter='\013', null='',
-                     encoding='UTF-8'):
+def get_conn(
+        dbname,
+        username,
+        password,
+        encoding='UTF-8',
+        ):
     conn = psycopg2.connect(database=dbname, user=username, password=password)
     conn.set_client_encoding(encoding)
+    return conn
+
+
+def get_cursor(
+        dbname,
+        username,
+        password,
+        encoding='UTF-8',
+        ):
+    conn = get_conn(
+        dbname=dbname,
+        username=username,
+        password=password,
+        encoding=encoding
+        )
     cur = conn.cursor()
-    with open(output_file_path, 'wt', encoding=encoding, newline='\n') as output_file:
-        cur.copy_to(output_file, table_name, sep=delimiter, null=null)
+    return cur
 
 
 def psycopg2_extract_using_bind(
@@ -15,11 +33,31 @@ def psycopg2_extract_using_bind(
         table_name,
         output_file_path, delimiter='\013',
         null='',
-        encoding='UTF-8'
-    ):
+        encoding='UTF-8'):
     conn = bind.engine.raw_connection()
     conn.set_client_encoding(encoding)
     cur = conn.cursor()
     with open(output_file_path, 'wt', encoding=encoding, newline='\n') as output_file:
         cur.copy_to(output_file, table_name, sep=delimiter, null=null)
     conn.close()
+
+
+def psycopg2_sql_extract(
+        bind,
+        sql,
+        output_file_path,
+        delimiter='\013',
+        null='',
+        encoding='UTF-8'):
+    conn = bind.engine.raw_connection()
+    conn.set_client_encoding(encoding)
+    cur = conn.cursor()
+
+    copy_stmt = "COPY ({sql}) TO STDOUT WITH DELIMITER {delimiter} NULL {null}".format(
+        sql=sql,
+        delimiter=delimiter,
+        null=null,
+    )
+
+    with open(output_file_path, 'wt', encoding=encoding, newline='\n') as output_file:
+        cur.copy_expert(copy_stmt, output_file)
