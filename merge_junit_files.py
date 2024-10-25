33310970e26b6a979a3540b1bb0f6d529b916eb0
Derek Wood <85i9-5ri3@spamex.com>
2024-10-25 16:03:31 -0400

    Tox: Fix requests import error, move junit files

--- merge_junit_files.py
+++ merge_junit_files.py
@@ -1,49 +1,41 @@
 from pathlib import Path
+from typing import Dict, List
 
 from junitparser import JUnitXml
-from junit2htmlreport.runner import run as junit2html_run
+try:
+    from junit2htmlreport.runner import run as junit2html_run
+except (ImportError, ModuleNotFoundError):
+    junit2html_run = None
+
 
 def merge_junit_files():
-    root = Path(__file__).parent
-    main_filename = 'junit.xml'
-    main_path = root / main_filename
-    # if main_path.exists():
-    #     main_xml = JUnitXml.fromfile(str(main_path))
-    # else:
-    #     main_xml = JUnitXml()
+    # TODO: Move existing junit files into subdir below
+
+    root = Path(__file__).parent / 'junit'
+    root.mkdir(exist_ok=True)
     for xml_file in root.glob('*.xml'):
-        if xml_file != main_path:
-            print(f"Updating {xml_file}")
+        # if xml_file.name != 'junit-py312-sqlalchemy20-slack_sdk-cw.xml':
+        #     continue
+        print(f"Updating {xml_file}")
+        xml_file_backup = xml_file.with_suffix('.xml-backup')
+        if xml_file_backup.exists():
+            xml = JUnitXml.fromfile(str(xml_file_backup))
+        else:
             xml = JUnitXml.fromfile(str(xml_file))
-            needs_save = False
-            names = dict()
-            for suite in xml:
-                # if suite in main_xml:
-                #     print("Starting with a clean JUnitXml")
-                #     main_xml = JUnitXml()
-                for case in suite:
-                    classname = case.classname
-                    if '/' in classname:
-                        _, classname = classname.split('/')
-                    class_parts = classname.split('.')
-                    classname = class_parts[-1]
-                    if classname in names:
-                        classname = '.'.join(class_parts)
-                    names[classname] = class_parts
-                    needs_save = True
-                    case.classname = f"{xml_file.stem}.{classname}"
-            # main_xml += xml
-            # xml_file.unlink()
-            if needs_save:
-                xml.write()
-            html_file = xml_file.with_suffix('.html')
+            xml.write(xml_file_backup, pretty=True)
+        for suite in xml:
+            for case in suite:
+                classname = case.classname
+                class_parts = classname.split('.')
+                classname = class_parts[-1]
+                case.classname = f"{xml_file.stem}.{classname}"
+        xml.write(xml_file, pretty=True)
+        html_file = xml_file.with_suffix('.html')
 
-            # if not html_file.exists():
-            if True:
-                # junit2html_run([str(xml_file), str(html_file), '--summary-matrix', '--max-failures', '1'])
+        if junit2html_run is not None:
+            if not html_file.exists() or html_file.stat().st_mtime < xml_file.stat().st_mtime:
+                print(f"Generating HTML report for {xml_file}")
                 junit2html_run([str(xml_file), str(html_file)])
-
-    # main_xml.write(str(root / main_filename))
 
 
 if __name__ == '__main__':
